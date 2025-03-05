9ca8694c586c45e2eecaf27640f68dad0a4011a6
Dhruva <dhruvatej6565@gmail.com>
2025-03-05 23:45:36 +0530

    DB connected

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -17,6 +18,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 5000;
+
+// MongoDB Connection String
+const MONGO_URI = "mongodb+srv://workdhruvateja:tqaNHUnMEYGxcyjY@project-db.t99lx.mongodb.net/?retryWrites=true&w=majority&appName=project-db";
+
+// Connect to MongoDB with Debugging
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log("MongoDB Connected Successfully!");
+})
+.catch((err) => {
+    console.error("MongoDB Connection Error:", err);
+});
 
 // Start the server
 app.listen(PORT, () => {
